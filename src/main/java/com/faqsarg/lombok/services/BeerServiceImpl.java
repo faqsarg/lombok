62de3b3484eed357e3b6bb75cd566104bc3fa4f3
faqsarg <fsargiottodev@gmail.com>
2024-06-25 02:39:49 -0300

    added missing @Service annotation for the service

--- src/main/java/com/faqsarg/lombok/services/BeerServiceImpl.java
+++ src/main/java/com/faqsarg/lombok/services/BeerServiceImpl.java
@@ -3,11 +3,13 @@ package com.faqsarg.lombok.services;
 import com.faqsarg.lombok.model.Beer;
 import com.faqsarg.lombok.model.BeerStyle;
 import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Service
 public class BeerServiceImpl implements BeerService{
     @Override
     public Beer getBeerById(UUID id) {
