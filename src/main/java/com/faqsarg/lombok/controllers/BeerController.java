f4c18823e4a746bc06a6e66f072d29a7f8032f4b
faqsarg <sargiottofacu@gmail.com>
2024-07-06 15:17:00 -0300

    added headers to the POST response

--- src/main/java/com/faqsarg/lombok/controllers/BeerController.java
+++ src/main/java/com/faqsarg/lombok/controllers/BeerController.java
@@ -5,6 +5,7 @@ import com.faqsarg.lombok.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,12 @@ public class BeerController {
     @PostMapping
     public ResponseEntity handlePost(@RequestBody Beer beer) {
         Beer savedBeer = beerService.saveNewBeer(beer);
-        return new ResponseEntity(HttpStatus.CREATED);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
