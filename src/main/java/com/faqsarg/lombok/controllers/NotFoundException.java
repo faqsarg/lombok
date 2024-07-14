cdba30b8b1737ef362f26c8629ef33395de50674
faqsarg <sargiottofacu@gmail.com>
2024-07-14 18:39:57 -0300

    replaced the class with @ControllerAdvice, with @ResponseStatus on the exception class

--- src/main/java/com/faqsarg/lombok/controllers/NotFoundException.java
+++ src/main/java/com/faqsarg/lombok/controllers/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.faqsarg.lombok.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "value not found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
         super();
